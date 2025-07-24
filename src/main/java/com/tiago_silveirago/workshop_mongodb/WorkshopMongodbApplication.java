55344f4eaf4aab8911f5a47ed782335f97b3697a
Tiago Silveira <tiago.silveirago@gmail.com>
2025-07-24 19:55:15 -0300

    setup: initial project with Spring Boot and MongoDB dependency

---
+++ src/main/java/com/tiago_silveirago/workshop_mongodb/WorkshopMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.tiago_silveirago.workshop_mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiago_silveirago/workshop_mongodb/WorkshopMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiago_silveirago.workshop_mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
